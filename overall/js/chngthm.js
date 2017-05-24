b87c6a77162e4ef01beb29a6623394a3f79ccc83
InkPrism <contact@dotsilver.net>
2017-05-24 23:24:58 +0200

    Cleaned up 'overall/js/chngthm.js'

--- overall/js/chngthm.js
+++ overall/js/chngthm.js
@@ -1,8 +1,10 @@
+var main_light = "<link id=\"theme\" rel=\"stylesheet\" href=\"http:\/\/dotsilver.net\/overall\/css\/main_light.css\">";
+var main_dark = "<link id=\"theme\" rel=\"stylesheet\" href=\"http:\/\/dotsilver.net\/overall\/css\/main_dark.css\">";
 document.getElementById("chngthm").addEventListener("click",_chngthm);
 function _chngthm(){
-	if(document.getElementById("theme").outerHTML == "<link id=\"theme\" rel=\"stylesheet\" href=\"http:\/\/dotsilver.net\/overall\/css\/main_light.css\">"){
-		document.getElementById("theme").outerHTML = "<link id=\"theme\" rel=\"stylesheet\" href=\"http:\/\/dotsilver.net\/overall\/css\/main_dark.css\">"
+	if(document.getElementById("theme").outerHTML == main_light){
+		document.getElementById("theme").outerHTML = main_dark
 	} else {
-		document.getElementById("theme").outerHTML = "<link id=\"theme\" rel=\"stylesheet\" href=\"http:\/\/dotsilver.net\/overall\/css\/main_light.css\">"
+		document.getElementById("theme").outerHTML = main_light
 	}
 }
