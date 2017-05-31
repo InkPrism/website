1ec77ca4e1c9f9b7892060d242af178cc7ed5856
InkPrism <contact@dotsilver.net>
2017-05-31 13:48:34 +0200

    No longer using 'outerHTML' in 'chngthm.js'

--- overall/js/chngthm.js
+++ overall/js/chngthm.js
@@ -1,10 +1,10 @@
-var main_light = "<link id=\"theme\" rel=\"stylesheet\" href=\"http:\/\/dotsilver.net\/overall\/css\/main_light.css\">";
-var main_dark = "<link id=\"theme\" rel=\"stylesheet\" href=\"http:\/\/dotsilver.net\/overall\/css\/main_dark.css\">";
+var main_light = "http:\/\/dotsilver.net\/overall\/css\/main_light.css";
+var main_dark = "http:\/\/dotsilver.net\/overall\/css\/main_dark.css";
 document.getElementById("chngthm").addEventListener("click",_chngthm);
 function _chngthm(){
-	if(document.getElementById("theme").outerHTML == main_light){
-		document.getElementById("theme").outerHTML = main_dark
+	if(document.getElementById("theme").getAttribute('href') == main_light){
+		document.getElementById("theme").setAttribute('href', main_dark)
 	} else {
-		document.getElementById("theme").outerHTML = main_light
+		document.getElementById("theme").setAttribute('href', main_light)
 	}
 }
