4aced55787c65ba96da1933b6d306174f5b6eab3
InkPrism <contact@dotsilver.net>
2017-06-02 20:55:58 +0200

    Even more vars

--- overall/js/chngthm.js
+++ overall/js/chngthm.js
@@ -1,10 +1,11 @@
 var main_light = "http:\/\/dotsilver.net\/overall\/css\/main_light.css";
 var main_dark = "http:\/\/dotsilver.net\/overall\/css\/main_dark.css";
+var theme = document.getElementById("theme");
 document.getElementById("chngthm").addEventListener("click",_chngthm);
 function _chngthm(){
-	if(document.getElementById("theme").getAttribute('href') == main_light){
-		document.getElementById("theme").setAttribute('href', main_dark)
+	if(theme.getAttribute('href') == main_light){
+		theme.setAttribute('href', main_dark);
 	} else {
-		document.getElementById("theme").setAttribute('href', main_light)
+		theme.setAttribute('href', main_light);
 	}
 }
