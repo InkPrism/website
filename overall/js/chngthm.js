6f035a31e4c0e4f4fc6f1e62a26157b6a552bad7
InkPrism <contact@dotsilver.net>
2017-08-28 06:59:16 +0200

    Mind the relative links.

--- overall/js/chngthm.js
+++ overall/js/chngthm.js
@@ -3,7 +3,7 @@ var main_dark = "http:\/\/dotsilver.net\/overall\/css\/main_dark.css";
 var theme = document.getElementById("theme");
 document.getElementById("chngthm").addEventListener("click",_chngthm);
 function _chngthm(){
-	if(theme.getAttribute('href') == main_light){
+	if(theme.getAttribute('href').includes("main_light.css")){
 		theme.setAttribute('href', main_dark);
 	} else {
 		theme.setAttribute('href', main_light);
